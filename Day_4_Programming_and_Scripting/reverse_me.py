0c5428d7129754fd41c04707ca84ebf32a9a1716
Roman Bohuk <netspark2@users.noreply.github.com>
2016-07-19 15:20:26 -0400

    Create reverse_me.py

---
+++ Day_4_Programming_and_Scripting/reverse_me.py
@@ -0,0 +1,17 @@
+def check_password(password):
+    if len(password) == 6 and password[0] == "m":
+        new = password[::-1]
+        if new[0] == "y" and new[1] == "e":
+            if password[1:4].upper() == "ONK":
+                if new.lower() == new:
+                    return "The password is correct!"
+        
+    return "Invalid password"
+
+while True:
+    password = raw_input("Please enter your password: ")
+    result = check_password(password)
+    print result
+    print
+    if result == "The password is correct!":
+        break
